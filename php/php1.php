8ef6e93ac64992482834ea50d4461fc28ef32077
sho <sskmy1024.y@gmail.com>
2020-06-01 23:58:41 +0900

    fix iketenai code

--- php/php1.php
+++ php/php1.php
@@ -1,10 +1,13 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
 	<meta charset='UTF-8'>
 	<title>課題1. Hello World</title>
 </head>
+
 <body>
-	<h1><?= "Hello World" ?></h1>
+	<h1><?= "???? Hello World" ?></h1>
 </body>
+
 </html>
